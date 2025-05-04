d4470ed0591edcedbac25bda735fc6e8872ab41f
Heng <31088159+shiukaheng@users.noreply.github.com>
2025-05-04 15:04:52 +0100

    vectorized policy and value iteration

--- src/rl_david_silver_2015/lecture_3.py
+++ src/rl_david_silver_2015/lecture_3.py
@@ -5,8 +5,8 @@ We demonstrate what we can do given a MDP (Markov Decision Process) and a policy
 
 from rl_david_silver_2015.gridworld2d import create_gridworld2d
 from rl_david_silver_2015.gridworld2d_tui import gridworld2d_policy_rollout_tui
-from rl_david_silver_2015.policy_iteration import policy_iteration
-from rl_david_silver_2015.value_iteration import value_iteration
+from rl_david_silver_2015.policy_iteration import policy_iteration_vectorized
+from rl_david_silver_2015.value_iteration import value_iteration_vectorized
 
 if __name__ == "__main__":
 
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     mdp.validate()
 
     # Perform policy iteration
-    optimal_policy, optimal_value = policy_iteration(mdp)
-
-    optimal_value, optimal_policy = value_iteration(mdp)
-
+    optimal_policy, optimal_value = policy_iteration_vectorized(mdp)
     gridworld2d_policy_rollout_tui(mdp, optimal_policy, V=optimal_value, terminal_states=[(0, 0), (3, 3)])
+
+    optimal_value2, optimal_policy2 = value_iteration_vectorized(mdp)
+    gridworld2d_policy_rollout_tui(mdp, optimal_policy2, V=optimal_value2, terminal_states=[(0, 0), (3, 3)])
